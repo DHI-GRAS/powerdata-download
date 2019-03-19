666a0c5abfb68c25e19a1b07c42280746e5a8961
Jonas Sølvsteen <j08lue@gmail.com>
2019-03-19 20:55:59 +0100

    raise RuntimeError for failing link from entry

--- powerdata_download/download.py
+++ powerdata_download/download.py
@@ -4,16 +4,36 @@ from urllib.parse import urlparse
 import os
 
 
+def get_link_from_entry(entry, filetype):
+    """Get download link from query result"""
+    try:
+        return entry['outputs'][filetype]
+    except KeyError:
+        raise RuntimeError(
+            f'Unable to get download link for file type {filetype}. '
+            f'Entry is {entry}.'
+        )
+
+
 def download_entry(entry, download_dir, filetype='netcdf'):
-    data_link = _get_link_from_entry(entry, filetype)
+    """Download entry
+
+    Parameters
+    ----------
+    entry : dict
+        from query
+    download_dir : str
+        path to download directory
+    filetype : str
+        file type to download
+
+    Returns
+    -------
+    str
+        path to downloaded file
+    """
+    data_link = get_link_from_entry(entry, filetype)
     o = urlparse(data_link)
     local_filename = os.path.join(download_dir, os.path.basename(o.path))
     urlretrieve(data_link, local_filename)
     return local_filename
-
-
-def _get_link_from_entry(entry, filetype):
-    try:
-        return entry['outputs'][filetype]
-    except KeyError:
-        return []
