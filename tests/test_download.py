7262dc38ff10f3f5bdbd2d427c4a3c51c65833ba
panakouris <ppkouris@gmail.com>
2019-03-20 10:45:55 +0100

    Added more test cases

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 from powerdata_download import download, query
 
 
@@ -7,3 +8,17 @@ def test_download_query(tmpdir, download_kw):
     entry = query.get_entry(url)
     local_filename = download.download_entry(entry, download_dir=tmpdir)
     assert os.path.exists(local_filename)
+
+
+def test_wrong_filetype(tmpdir, download_kw):
+    url = query.build_query_url(**download_kw)
+    entry = query.get_entry(url)
+    with pytest.raises(RuntimeError):
+        download.download_entry(entry, download_dir=tmpdir, filetype='ascii')
+
+
+def test_wrong_query(tmpdir, download_kw):
+    download_kw['parameters'] = []
+    url = query.build_query_url(**download_kw)
+    with pytest.raises(RuntimeError):
+        query.get_entry(url)
